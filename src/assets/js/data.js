f1f14c58bc5bc14890ea36bef2a33e855d844b1b
JavierGR10 <javier.a.garcia.rom@gmail.com>
2024-03-22 20:46:13 -0600

    Data separed from components

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,54 +1,213 @@
-const users = [
-  {
-    username: "Abraham81",
-    password: "DyDXIOHHVv52YoN",
-    name: "Fannie Berge",
+const distributionData = {
+  LAJA: {
+    data: [
+      { name: "Nombre del ramal", value: "TRONCAL" },
+      { name: "Fecha", value: "21-sept-21" },
+      { name: "Voltaje primario del transformador (KV)", value: "69" },
+      { name: "Voltaje secundario del transformador (KV)", value: "23,8" },
+      { name: "Icc3f(B.T.) EN AMPERES", value: "3205" },
+      { name: "ANGULO (Icc3f)", value: "-88,2" },
+      { name: "Icc1f(B.T.) EN AMPERES", value: "3350" },
+      { name: "ANGULO (Icc1f)", value: "-88,8" },
+      { name: "MVAb", value: "100" },
+      { name: "Resistividad del terreno (ohms/mts.)", value: "100" },
+      { name: "Resistencia de contacto a tierra (ohms)", value: "0" },
+    ],
+    coordinates: [
+      {
+        lat: 20.578131,
+        lng: -103.147131,
+        node: "S.E. Puente Grandre a C-5186",
+      },
+      {
+        lat: 20.579809,
+        lng: -103.125593,
+        node: "C-5186 A CRUCERO AEREO",
+      },
+      {
+        lat: 20.580238,
+        lng: -103.124536,
+        node: "CRUCERO AEREO HASTA  F 132",
+      },
+      { lat: 20.589277, lng: -103.113087, node: "F 132 A RTA 6564" },
+      {
+        lat: 20.614783,
+        lng: -103.080317,
+        node: "C RTA 6564 A RTE 6526",
+      },
+    ],
   },
-  {
-    username: "Carole_Graham",
-    password: "tsf5ESe8qzvYuqC",
-    name: "Justice Kutch",
+  HUIZQUILCO: {
+    data: [
+      { name: "Nombre del ramal", value: "TRONCAL" },
+      { name: "Fecha", value: "05-mar-24" },
+      { name: "Voltaje primario del transformador (KV)", value: "115" },
+      { name: "Voltaje secundario del transformador (KV)", value: "23,8" },
+      { name: "Icc3f(B.T.) EN AMPERES", value: "2572" },
+      { name: "ANGULO (Icc3f)", value: "-87,3" },
+      { name: "Icc1f(B.T.) EN AMPERES", value: "2758" },
+      { name: "ANGULO (Icc1f)", value: "-88" },
+      { name: "MVAb", value: "100" },
+      { name: "Resistividad del terreno (ohms/mts.)", value: "100" },
+      { name: "Resistencia de contacto a tierra (ohms)", value: "0" },
+    ],
+    coordinates: [
+      {
+        lat: 20.61465,
+        lng: -103.07961,
+        node: "DE SUBESTACION A C381",
+      },
+      {
+        lat: 20.621969,
+        lng: -103.050027,
+        node: "DE C381 A  C309",
+      },
+      {
+        lat: 20.621893,
+        lng: -103.048886,
+        node: "C309 A C310",
+      },
+      { lat: 20.621746, lng: -103.042287, node: "C310 A CT 107" },
+      {
+        lat: 20.622891,
+        lng: -103.029398,
+        node: "DE CT107 A F4729",
+      },
+      {
+        lat: 20.622615,
+        lng: -103.0286,
+        node: "DE F4729 A F 8948",
+      },
+      {
+        lat: 20.622356,
+        lng: -103.022075,
+        node: "DE F8948 A F697 CROMADORA",
+      },
+      {
+        lat: 20.6182,
+        lng: -103.009369,
+        node: "DE F697 CROMADORA A RTA 597",
+      },
+      {
+        lat: 20.603641,
+        lng: -102.978259,
+        node: "DE RTA 597 A  F 3387 BECAR ",
+      },
+      {
+        lat: 20.601799,
+        lng: -102.974436,
+        node: "DE F 3387 A C0085",
+      },
+      {
+        lat: 20.585448,
+        lng: -102.927647,
+        node: "C0085 A RTI 598",
+      },
+      {
+        lat: 20.619164,
+        lng: -102.914146,
+        node: "DE RTI 598 A C1019",
+      },
+      {
+        lat: 20.619008,
+        lng: -102.908053,
+        node: "DE C1019 A CALLE HUMBERTO DE ANDA TECO",
+      },
+      {
+        lat: 20.63085,
+        lng: -102.897061,
+        node: "CALLE HUMBERTO DE ANDA A RTE 599",
+      },
+    ],
   },
-  {
-    username: "Raven_Gleichner10",
-    password: "gVOmUH_oBioQOeS",
-    name: "Carson Wolf",
+  CALDERON: {
+    data: [
+      { name: "Nombre del ramal", value: "TRONCAL" },
+      { name: "Fecha", value: "06-mar-24" },
+      { name: "Voltaje primario del transformador (KV)", value: "115" },
+      { name: "Voltaje secundario del transformador (KV)", value: "23,8" },
+      { name: "Icc3f(B.T.) EN AMPERES", value: "2572" },
+      { name: "ANGULO (Icc3f)", value: "-87,3" },
+      { name: "Icc1f(B.T.) EN AMPERES", value: "2758" },
+      { name: "ANGULO (Icc1f)", value: "-88" },
+      { name: "MVAb", value: "100" },
+      { name: "Resistividad del terreno (ohms/mts.)", value: "100" },
+      { name: "Resistencia de contacto a tierra (ohms)", value: "0" },
+    ],
+    coordinates: [
+      {
+        lat: 20.617411,
+        lng: -103.069829,
+        node: "S.E. ZAD A  F5310",
+      },
+      {
+        lat: 20.617921,
+        lng: -103.066771,
+        node: "DE F 5310 A C298",
+      },
+      {
+        lat: 20.618485,
+        lng: -103.06307,
+        node: "C298 A C285",
+      },
+      {
+        lat: 20.621782,
+        lng: -103.054241,
+        node: "DE C285 A C408",
+      },
+      {
+        lat: 20.622626,
+        lng: -103.053798,
+        node: "C408 A C409",
+      },
+      {
+        lat: 20.623155,
+        lng: -103.053643,
+        node: "C409 A F 4842",
+      },
+      {
+        lat: 20.641264,
+        lng: -103.050094,
+        node: "DE F4842 A F6408 3D",
+      },
+      {
+        lat: 20.651571,
+        lng: -103.044717,
+        node: "F6408 3D A RTA 553",
+      },
+      {
+        lat: 20.655184,
+        lng: -103.033594,
+        node: "DE RTA 553 A F4848",
+      },
+      {
+        lat: 20.664353,
+        lng: -103.013625,
+        node: "DE F4848 A F4849 3D SAN JOSE DE LAS F",
+      },
+      {
+        lat: 20.681602,
+        lng: -103.00104,
+        node: "DE LA F 4849 DE 3D A F5314 3D LA PURISIMA",
+      },
+      {
+        lat: 20.685702,
+        lng: -102.994391,
+        node: "F5314 3D LA PURISIMA A C415",
+      },
+      {
+        lat: 20.687306,
+        lng: -102.991811,
+        node: "C415 A F 163 3D LA JOYA",
+      },
+      {
+        lat: 20.694556,
+        lng: -102.980722,
+        node: "DE F 163 A RTE 408",
+      },
+    ],
   },
-  {
-    username: "Kevin67",
-    password: "sOwhLSSJBvD2hfz",
-    name: "Julie Smith",
-  },
-  {
-    username: "Dessie71",
-    password: "kholRIi8xdFKJYe",
-    name: "Juliet Monahan",
-  },
-  {
-    username: "Tremaine_Fahey",
-    password: "M9J5q8U9CmkMdUw",
-    name: "Otha Zboncak",
-  },
-  {
-    username: "Fae_Prohaska68",
-    password: "kYXBkKsSzqyYoTr",
-    name: "Hazel Stracke",
-  },
-  {
-    username: "Diana_OKon",
-    password: "ZUSMMd_XKdolfZC",
-    name: "Reggie Lockman",
-  },
-  {
-    username: "Noemi_Mohr44",
-    password: "mcST0orfs4B7vA2",
-    name: "Jalon O'Keefe",
-  },
-  {
-    username: "Jerald_Bernier",
-    password: "32Q0ufmRp2YViaV",
-    name: "Hannah Green",
-  },
-];
+};
 
-export default users;
+
+export default distributionData;
---
+++ src/assets/js/selectData.js
@@ -0,0 +1,111 @@
+const selectData = {
+  zones: [
+    
+    {
+      value: "SECTOR REFORMA",
+      circuitName: [
+        { label: "Circuito 1", value: "1" },
+        { label: "Circuito 2", value: "2" },
+        { label: "Circuito 3", value: "3" },
+      ],
+    },
+    {
+      value: "SECTOR HIDALGO",
+      circuitName: [
+        { label: "Circuito 4", value: "4" },
+        { label: "Circuito 5", value: "5" },
+        { label: "Circuito 6", value: "6" },
+      ],
+    },
+    {
+      value: "SECTOR JUAREZ",
+      circuitName: [
+        { label: "Circuito 4", value: "4" },
+        { label: "Circuito 5", value: "5" },
+        { label: "Circuito 6", value: "6" },
+      ],
+    },
+    {
+      value: "SECTOR LIBERTAD",
+      circuitName: [
+        { label: "Circuito 4", value: "4" },
+        { label: "Circuito 5", value: "5" },
+        { label: "Circuito 6", value: "6" },
+      ],
+    },
+    {
+      value: "ZONA CHAPALA",
+      circuitName: [
+        { label: "PTD-5235 LA LAJA", value: "LAJA" },
+        { label: "ZAD-5135 HUIZQUILCO", value: "HUIZQUILCO" },
+        { label: "ZAD-5125 CALDERON", value: "CALDERON" },
+      ],
+    },
+    {
+      value: "ZONA MINAS",
+      circuitName: [
+        { label: "Circuito 4", value: "4" },
+        { label: "Circuito 5", value: "5" },
+        { label: "Circuito 6", value: "6" },
+      ],
+    },
+    {
+      value: "ZONA COSTA",
+      circuitName: [
+        { label: "Circuito 4", value: "4" },
+        { label: "Circuito 5", value: "5" },
+        { label: "Circuito 6", value: "6" },
+      ],
+    },
+    {
+      value: "ZONA ALTOS",
+      circuitName: [
+        { label: "Circuito 4", value: "4" },
+        { label: "Circuito 5", value: "5" },
+        { label: "Circuito 6", value: "6" },
+      ],
+    },
+    {
+      value: "ZONA CIENEGA",
+      circuitName: [
+        { label: "Circuito 4", value: "4" },
+        { label: "Circuito 5", value: "5" },
+        { label: "Circuito 6", value: "6" },
+      ],
+    },
+    {
+      value: "ZONA TEPIC",
+      circuitName: [
+        { label: "Circuito 4", value: "4" },
+        { label: "Circuito 5", value: "5" },
+        { label: "Circuito 6", value: "6" },
+      ],
+    },
+    {
+      value: "ZONA SANTIAGO",
+      circuitName: [
+        { label: "Circuito 4", value: "4" },
+        { label: "Circuito 5", value: "5" },
+        { label: "Circuito 6", value: "6" },
+      ],
+    },
+    {
+      value: "ZONA VALLARTA ",
+      circuitName: [
+        { label: "Circuito 4", value: "4" },
+        { label: "Circuito 5", value: "5" },
+        { label: "Circuito 6", value: "6" },
+      ],
+    },
+    {
+      value: "ZONA ZAPOTLAN",
+      circuitName: [
+        { label: "Circuito 4", value: "4" },
+        { label: "Circuito 5", value: "5" },
+        { label: "Circuito 6", value: "6" },
+      ],
+    },
+  ],
+};
+
+export default selectData;
